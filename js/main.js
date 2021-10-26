5c203a5345867b61e6d6acb519e1abbd9563c911
Dario0122 <dario.di.cuia@gmail.com>
2021-10-26 17:33:09 +0200

    dadi

--- js/main.js
+++ js/main.js
@@ -13,11 +13,38 @@ const mailList = ['dario.di.cuia@gmail.com', 'boolean@outlook.com', 'array.js@vi
 console.log(mailList);
 console.log('numero di array', mailList.length)
 
-const email = parseInt ( prompt('Inserisci la tua email') );
+const email = prompt('Inserisci la tua email') ;
 console.log(email);
 
-if( (email < 0) || (email < mailList.length) ){
-    console.log("L'email inserita è valida");
-} else{
-    console.log("l'email inserita non è valida");
+// if( (email < 0) || (email < mailList.length) ){
+//     console.log("L'email inserita è valida");
+// } else{
+//     console.log("l'email inserita non è valida");
+// }
+
+for(let i = 0; i < mailList.length; i++){
+    if(email == mailList[i]){
+        console.log('email valida');
+    } else if(email != mailList[i]){
+        console.log('email non valida');
+    }
 }
+
+//GIOCO DEI DADI
+const btn = document.querySelector('.gen-btn')
+const rand1 = document.querySelector('.gen-random1')
+const rand2 = document.querySelector('.gen-random2')
+const dadi = document.querySelector('.dadi')
+
+btn.addEventListener('click', function(){
+    const rand1 = Math.floor( Math.random()*6 ) + 1;
+    console.log(rand1, 'giocatore');
+    const rand2 = Math.floor( Math.random()*6 ) + 1;
+    console.log(rand2, 'computer');
+
+    if(rand1 > rand2){
+        console.log('Hai vinto!');
+    } else{
+        console.log('Hai perso!'); 
+    }
+});
